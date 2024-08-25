1e3142d3242570225edcd875cb1ff018a780e035
Batuhan <batuhanozer1042@gmail.com>
2024-08-25 14:32:06 +0300

    configure asgi

--- ChatApp/asgi.py
+++ ChatApp/asgi.py
@@ -10,7 +10,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
 
+django_asgi_app = get_asgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    # Just HTTP for now. (We can add other protocols later.)
+})
+
+ASGI_APPLICATION = 'ChatApp.asgi.application'
+
+
