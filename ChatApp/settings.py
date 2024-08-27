d22973989ef8314fd80eb2858d4930384efcbfef
Batuhan <batuhanozer1042@gmail.com>
2024-08-27 14:27:35 +0300

    Configured asgi

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -126,3 +126,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'chat.User'
+
+ASGI_APPLICATION = "chat.asgi.application"
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
