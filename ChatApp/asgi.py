f7a96b41a6d775007b252de7981bab49ee5b3466
Batuhan <batuhanozer1042@gmail.com>
2024-08-30 15:06:37 +0300

    websocket_urlpatterns

--- ChatApp/asgi.py
+++ ChatApp/asgi.py
@@ -10,14 +10,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat.routing import websocket_urlpatterns
 
 django_asgi_app = get_asgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(URLRouter(
+        websocket_urlpatterns
+    )),
 })
 
 ASGI_APPLICATION = 'ChatApp.asgi.application'
