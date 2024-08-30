c62b1e8af97d89bd276d693676e37be97d41cc29
Batuhan <batuhanozer1042@gmail.com>
2024-08-30 17:17:01 +0300

    routing for synchronus

--- chat/urls.py
+++ chat/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('chat/<int:chat_id>/', views.chat, name='chat'),
+    path('search_users/', views.search_users, name='search_users'),
+    path('start_chat/<int:user2_id>/', views.start_chat, name='start_chat'),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
